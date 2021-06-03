02eea9f7a9e1a0c268486808c033dc03dd69171a
Daniil Cherepanov <glunder124@gmail.com>
2021-06-03 23:05:47 +0300

    Added benchmark

---
+++ LineCompareBenchmark/Benchmark.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using LineCompare;
+
+namespace LineCompareBenchmark
+{
+    public class Benchmark
+    {
+        private const string FirstFileName = "1.txt";
+        private const string SecondFileName = "2.txt";
+
+        [Params(1000, 50000, 1000000)]
+        public int FileLength;
+        
+        [Params(10, 50)]
+        public int EntryCount;
+
+        [Params(10, 50)]
+        public int EntryLength;
+
+        [ParamsSource(nameof(GetCompareMethods))]
+        public string CompareMethod;
+
+        public static IEnumerable<string> GetCompareMethods()
+        {
+            return ComparatorLibrary.GetComparatorNames();
+        }
+        
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            FileUtils.CreateTestFile(FirstFileName, FileLength, EntryCount, EntryLength);
+            FileUtils.CreateTestFile(SecondFileName, FileLength, EntryCount, EntryLength);
+        }
+        
+        [Benchmark]
+        public int Run()
+        {
+            var comparator = ComparatorLibrary.GetComparator(CompareMethod, FirstFileName, SecondFileName);
+            comparator.Compare().Wait();
+            return comparator.GetMissingInFirst().Count() + comparator.GetMissingInSecond().Count();
+        }
+        
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            FileUtils.DeleteTestFile(FirstFileName);
+            FileUtils.DeleteTestFile(SecondFileName);
+        }
+    }
+}
---
+++ LineCompareBenchmark/FileUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LineCompareBenchmark
+{
+    public static class FileUtils
+    {
+        private const string Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        private static readonly Random Rnd = new Random();
+
+        private static char GenerateChar()
+        {
+            return Chars[Rnd.Next(Chars.Length)];
+        }
+        
+        private static string GenerateLine(int entryCount, int entryLength)
+        {
+            var count = entryCount * (entryLength + 1) - 1;
+            var arr = new char[count];
+            for (var i = 0; i < count; i++)
+            {
+                arr[i] = (i + 1) % (entryLength + 1) == 0 ? ',' : GenerateChar();
+            }
+            return new string(arr);
+        }
+
+        private static IEnumerable<string> GenerateFile(int length, int entryCount, int entryLength)
+        {
+            for (var i = 0; i < length; i++)
+            {
+                yield return GenerateLine(entryCount, entryLength);
+            }
+        }
+
+        public static void CreateTestFile(string path, int length, int entryCount, int entryLength)
+        {
+            File.WriteAllLines(path, GenerateFile(length, entryCount, entryLength));
+        }
+
+        public static void DeleteTestFile(string path)
+        {
+            File.Delete(path);
+        }
+    }
+}
